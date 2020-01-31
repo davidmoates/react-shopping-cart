bb82bf4694b05d40d81c1a6b6f1ae58b555e0f1d
David Moates <david.m.moates@gmail.com>
2020-01-30 22:55:21 -0600

    update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 	};
 
 	return (
-		<ProductContext.Provider value{{ produts, addItem }}>
+		<ProductContext.Provider value={{ products, addItem }}>
 			<div className="App">
 				<Navigation cart={cart} />
 
